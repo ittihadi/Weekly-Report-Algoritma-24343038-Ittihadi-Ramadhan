57cc5047376da668a0ba65febfdb90d0593340dc
Hadi <rittihadi@gmail.com>
2024-11-17 08:12:25 +0700

    Perbarui Modul 7.3
    Tambahkan masukan username

--- Modul 7 Array/jobsheet7_3.c
+++ Modul 7 Array/jobsheet7_3.c
@@ -6,6 +6,16 @@ int main()
     char kunci[] = "waah keren banget";
     char input[32];
 
+    printf("Silahkan masukkan username: ");
+    scanf("%31[^\n]s", input);
+    getchar();
+
+    if (strcmp(input, "admin") != 0)
+    {
+        printf("Pengguna %s tidak terdaftar\n", input);
+        return 0;
+    }
+
     printf("Silahkan masukkan password: ");
     scanf("%31[^\n]s", input);
     getchar();
